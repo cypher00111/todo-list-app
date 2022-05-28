793e8afefd4379467e4e5f2479e7725a9ae1e8be
[cypher] <migangkorajkonwar@gmail.com>
2022-05-29 00:42:49 +0530

    addfiles

---
+++ date.js
@@ -0,0 +1,12 @@
+module.exports = getDate;
+
+function getDate() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return today.toLocaleDateString("en-US", options);
+}

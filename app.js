e00ac1a6a9678193ff4c4793bfeb89161334105b
[cypher] <migangkorajkonwar@gmail.com>
2022-06-18 01:32:58 +0530

    upgrade
    upgrade in adding mongoose

--- app.js
+++ app.js
@@ -1,29 +1,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-const items = ["Food", "Water", "House"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome",
+});
+
+const item2 = new Item({
+  name: "Welcome 1",
+});
+
+const item3 = new Item({
+  name: "Welcome 2",
+});
+
+const defaultItems = [item1, item2, item3];
+
 app.get("/", function (req, res) {
-  const day = date();
-  res.render("list", { listTitle: day, newListItems: items });
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Login success");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+  console.log(req.body);
 });
 
 app.get("/work", function (req, res) {
